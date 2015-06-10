99e466825cff89cf4bee124799187b0393189e1e
Alberto Bellotti <abellott@redhat.com>
2015-06-10 13:16:11 -0400

    - Added --wait option
    - Adding --wait option to vncviewer_build.sh

--- bin/vncviewer_build.sh
+++ bin/vncviewer_build.sh
@@ -1,10 +1,37 @@
-export VMID=`sudo virsh list | grep running | awk '{print $1}'`
-if [ -z "${VMID}" ]; then
-  echo "Build VM not started yet ..."
-  exit 1
+#!/bin/bash
+
+SLEEP=5
+WAIT=0
+if [ "$1" = "--wait" ]
+then
+  WAIT=1
+  shift
 fi
-echo "Build VM ID: $VMID"
-export VNCID=`sudo virsh domdisplay $VMID | cut -f3 -d:`
-echo "VNC ID :${VNCID}"
-echo "Bringing up VncViewer for :${VNCID}   Note: Menu Key is F5 ..."
-nohup vncviewer -MenuKey F5 :$VNCID > vncviewer.out 2>&1 &
+
+while :
+do
+  VMSYS="`sudo virsh list | grep running`"
+  if [ -z "$VMSYS" -o -n "`echo $VMSYS | grep guestfs`" ]
+  then
+    echo "Build VM not started yet ..."
+    if [ "$WAIT" = 0 ]; then exit 1; fi
+    sleep $SLEEP
+    continue
+  fi
+
+  export VMID=`echo $VMSYS | awk '{print $1}'`
+  if [ -z "${VMID}" ]; then
+    echo "Build VM not started yet ..."
+    if [ "$WAIT" = 0 ]; then exit 1; fi
+    sleep $SLEEP
+    continue
+  fi
+
+  echo "Build VM ID: $VMID"
+  export VNCID=`sudo virsh domdisplay $VMID | cut -f3 -d:`
+
+  echo "VNC ID :${VNCID}"
+  echo "Bringing up VncViewer for :${VNCID}   Note: Menu Key is F5 ..."
+  nohup vncviewer -MenuKey F5 :$VNCID >> /build/logs/vncviewer.out 2>&1 &
+  exit 0
+done
