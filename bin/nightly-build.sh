6847bd9adab9b2dd57a25a05e5be752b32d23683
Alberto Bellotti <abellott@redhat.com>
2015-06-26 13:59:32 -0400

    New build structure
    - Update bin scripts to be /build/manageiq/bin
    - Update documentation to reflect cloning manageiq-appliance-build
      and setup symlinks from /build/bin to /build/manageiq/bin
      and /build/scripts to /build/manageiq/scripts

--- bin/nightly-build.sh
+++ bin/nightly-build.sh
@@ -30,9 +30,9 @@ if [ "${1}" = "--fg" ]
 then
   echo "Nightly Build kicked off, Log being saved in ${LOG_FILE} ..."
 
-  time ruby /build/scripts/vmbuild.rb $BUILD_OPTIONS 2>&1 | tee ${LOG_FILE}
+  time ruby /build/manageiq/scripts/vmbuild.rb $BUILD_OPTIONS 2>&1 | tee ${LOG_FILE}
 else
-  nohup time ruby /build/scripts/vmbuild.rb $BUILD_OPTIONS >${LOG_FILE} 2>&1 &
+  nohup time ruby /build/manageiq/scripts/vmbuild.rb $BUILD_OPTIONS >${LOG_FILE} 2>&1 &
 
   echo "Nightly Build kicked off, Log @ ${LOG_FILE} ..."
 fi
