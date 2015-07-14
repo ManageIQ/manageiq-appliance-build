1fd1859ac8f0e20a6ade4fe4ed8537f11ee9212c
Joe Rafaniello <jrafanie@redhat.com>
2015-07-14 11:15:45 -0400

    Delete temp qcow2 images and keep only 1 day of temp images.

--- bin/clean_imagefactory_storage.sh
+++ bin/clean_imagefactory_storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-days_to_keep_storage=2
+days_to_keep_storage=1
 imgfac_storage=/build/storage
 
-find $imgfac_storage -type f -mtime +$days_to_keep_storage -regex ".*\.\(body\|meta\)$" | xargs rm -vf
+find $imgfac_storage -type f -mtime +$days_to_keep_storage -regex ".*\.\(body\|meta\|\.qcow2\)$" | xargs rm -vf
