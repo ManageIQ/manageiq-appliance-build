3d8a2025128e9eeaac8cc5c71f9651b4962225cd
Joe Rafaniello <jrafanie@redhat.com>
2015-07-16 10:41:34 -0400

    Clarify the -mtime +0 is within the past 24 hours, remove now badly named variable.

--- bin/clean_imagefactory_storage.sh
+++ bin/clean_imagefactory_storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-days_to_keep_storage=0 # keep only files modified within the past day
 imgfac_storage=/build/storage
 
-find $imgfac_storage -type f -mtime +$days_to_keep_storage -regex ".*\.\(body\|meta\|qcow2\)$" | xargs rm -vf
+# Note: -mtime +0 finds files within the past 24 hours
+find $imgfac_storage -type f -mtime +0 -regex ".*\.\(body\|meta\|qcow2\)$" | xargs rm -vf
