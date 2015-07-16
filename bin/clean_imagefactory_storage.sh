d8b0878977a103d701e7623310d98cd3eae1366e
Joe Rafaniello <jrafanie@redhat.com>
2015-07-16 10:17:25 -0400

    The period is before the paren already.  We want abc.qcow2 not abc..qcow2.

--- bin/clean_imagefactory_storage.sh
+++ bin/clean_imagefactory_storage.sh
@@ -3,4 +3,4 @@
 days_to_keep_storage=0 # keep only files modified within the past day
 imgfac_storage=/build/storage
 
-find $imgfac_storage -type f -mtime +$days_to_keep_storage -regex ".*\.\(body\|meta\|\.qcow2\)$" | xargs rm -vf
+find $imgfac_storage -type f -mtime +$days_to_keep_storage -regex ".*\.\(body\|meta\|qcow2\)$" | xargs rm -vf
