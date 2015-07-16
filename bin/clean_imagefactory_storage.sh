d5feb4e50ee303947db7422b3f1ed0c19c0cb626
Joe Rafaniello <jrafanie@redhat.com>
2015-07-16 10:14:41 -0400

    -mtime 1 is actually > 2 days, keep only files within the last 24 hours.

--- bin/clean_imagefactory_storage.sh
+++ bin/clean_imagefactory_storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-days_to_keep_storage=1
+days_to_keep_storage=0 # keep only files modified within the past day
 imgfac_storage=/build/storage
 
 find $imgfac_storage -type f -mtime +$days_to_keep_storage -regex ".*\.\(body\|meta\|\.qcow2\)$" | xargs rm -vf
