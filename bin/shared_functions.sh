ca2fb66411425cb8d2608f0c61cd9821f28bdc65
Satoe Imaishi <simaishi@redhat.com>
2019-10-11 17:50:28 -0400

    Change how to identify running build

--- bin/shared_functions.sh
+++ bin/shared_functions.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-stop_on_existing_build ()
+stop_on_existing_imagefactory_build ()
 {
   CUR_BUILD="`virsh list | sed '1,2d'`"
   if [ -n "${CUR_BUILD}" ]
   then
     echo "Current ImageFactory Build ongoing, cannot kick-off build"
+    exit 1
+  fi
+}
+
+stop_on_existing_build ()
+{
+  if [ "`pgrep -fa ${BUILD_DIR}\/.*/vmbuild.rb`" ]; then
+    echo "vmbuild.rb running from the current directory, cannot kick-off build"
     exit 1
   fi
 }
